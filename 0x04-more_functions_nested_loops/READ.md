3eb5081c55196952bab3a5deffc229be97d68f86
etzjewelz <jewelzdamano@gmail.com>
2023-08-18 15:39:56 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/READ.md
@@ -0,0 +1,3 @@
+More functions, More nested loops
+
+10 mandatory tasks and 2 advanced tasks
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef main_h
+#define main_h
+
 int _putchar(char c);
 
 int _isupper(int c);
@@ -21,3 +24,4 @@ void print_square(int size);
 void print_triangle(int size);
 
 void print_number(int n);
+#endif
